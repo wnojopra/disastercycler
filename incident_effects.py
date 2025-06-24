1ca093eff9e747d986d5444e406d412cd45ee076
Willy Nojopranoto <nojopranoto@gmail.com>
2025-06-24 02:30:23 +0000

    Typos and bug fixes

--- incident_effects.py
+++ incident_effects.py
@@ -18,20 +18,18 @@ def resolve_murder(culprit: Character, incident: Incident, game_state: GameState
         target_character = next((c for c in others if c.name == target), None)
         if target_character is None:
             raise ValueError(f"Murder target not in same location as Culprit")
-        victim = others[0]
-        victim.alive = False
+        target_character.alive = False
         print(f"💀 {culprit.name} (Murder Culprit) murdered {target_character.name} at {culprit.location.name}")
     else:
         print(f"🟢 The murder incident does not happen on day {game_state.day}")
 
-# Placeholder for other incidents
 def resolve_suicide(culprit: Character, incident: Incident, game_state: GameState):
     if not culprit.alive:
         print(f"🟢 The suicide incident does not happen on day {game_state.day}")
         return
     else:
         culprit.alive = False
-        print(f"💀 {culprit.name} comitted suicide")
+        print(f"💀 {culprit.name} committed suicide")
 
 INCIDENT_EFFECTS = {
     IncidentType.MURDER: resolve_murder,
